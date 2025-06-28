2c0c0cb9a897aaebc84309f5598b80da0b1b9f58
akalya007 <akalya.d2022eee@sece.ac.in>
2025-06-28 20:44:35 +0530

    cra & tailwind

---
+++ src/NOTES.md
@@ -0,0 +1,1 @@
+npm install ajv@8 ajv-keywords@5
---
+++ src/components/Body.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Body = () => {
+  return (
+    <div className='text-green-900 font-bold font-medium'>Akals , your life is in your hand , please avoid the Temporary distraction.</div>
+  )
+}
+
+export default Body
